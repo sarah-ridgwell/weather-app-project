dd663aa1601cdd9ffa3b2a80c3c12997f4b9a287
sarah-ridgwell <73703501+sarah-ridgwell@users.noreply.github.com>
2020-12-06 09:24:35 +0000

    CodeSandbox to Git
    Transferred code to git from Code Sandbox

---
+++ src/app.js
@@ -0,0 +1,115 @@
+function showWeather(response) {
+  let apiCityName = (document.querySelector("#city").innerHTML =
+    response.data.name);
+  let temperatureElement = (document.querySelector(
+    `#number`
+  ).innerHTML = Math.round(response.data.main.temp));
+  console.log(response);
+  let description = (document.querySelector("#description").innerHTML =
+    response.data.weather[0].description);
+}
+
+function search(city) {
+  let apiKey = "94b9970cd6a6ba265c6a061d30640040";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function submitCity(event) {
+  event.preventDefault();
+  let city = document.querySelector(`#city-input`).value;
+  search(city);
+}
+
+let cityInput = document.querySelector(`#search-form`);
+cityInput.addEventListener("submit", submitCity);
+
+function searchLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "94b9970cd6a6ba265c6a061d30640040";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(showWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocation = document.querySelector(`#location-button`);
+currentLocation.addEventListener("click", getCurrentLocation);
+
+function convertCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(`#number`);
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = 10;
+}
+
+function convertFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(`#number`);
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = 40;
+}
+
+let celsiusClick = document.querySelector(`#celsius`);
+celsiusClick.addEventListener("click", convertCelsius);
+
+let fahrenheitClick = document.querySelector(`#fahrenheit`);
+fahrenheitClick.addEventListener("click", convertFahrenheit);
+
+let dateNow = new Date();
+
+let months = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+let days = [
+  `Sunday`,
+  `Monday`,
+  `Tuesday`,
+  `Wednesday`,
+  `Thursday`,
+  `Friday`,
+  `Saturday`,
+];
+
+let currentDay = days[dateNow.getDay()];
+
+let currentMonth = months[dateNow.getMonth()];
+
+let hours = dateNow.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = dateNow.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentTime = document.querySelector("#current-time");
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${dateNow.getDate()} ${currentMonth}`;
+
+let weekDay = document.querySelector("#current-day");
+weekDay.innerHTML = `${currentDay}`;
+
+search("London");
